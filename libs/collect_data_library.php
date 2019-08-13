4a4296ec23ac0adfd5d56e6f28051887afcb714c
Felix <f.sommi@gmx.de>
2019-08-13 13:26:11 +0200

    basic structure Test of dom

--- libs/collect_data_library.php
+++ libs/collect_data_library.php
@@ -1,7 +1,34 @@
 <?php
 function collect_data($url) {
     //TODO
+    //XML analyse somethingb
+    $html = file_get_contents($url);
+    $name = collect_name($html);
+    $pic = collect_pic($html);
+
     return array($url, $url);
 }
+function collect_name($html) {
+    $dom = new DOMDocument;
+    $dom->loadHTML( $html );
+    $finder = new DomXPath($dom);
+    $classname="playerAvatar profile_header_size online";
+    $nodes = $finder->query("//*[contains(concat(' ', normalize-space(@class), ' '), ' $classname ')]");
+    var_dump($nodes);
 
+        //Try 2
+    $classname="playerAvatarAutoSizeInner";
+    $nodes = $finder->query("//*[contains(concat(' ', normalize-space(@class), ' '), ' $classname ')]");
+    var_dump($nodes);
+    return;
+}
+function collect_pic($html) {
+    $dom = new DOMDocument;
+    $dom->loadHTML( $html );
+    $finder = new DomXPath($dom);
+    $classname="actual_persona_name";
+    $nodes = $finder->query("//*[contains(concat(' ', normalize-space(@class), ' '), ' $classname ')]");
+    var_dump($nodes);
+    return;
+}
 ?>
